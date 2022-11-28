6e5eba4ecb223c9ce6434fd3e7196dce6761374f
MrDoAsILike <coolbreeze78@hotmail.com>
2022-11-28 14:00:54 -0500

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -77,6 +77,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ohryx = {
     isNormalUser = true;
+    home = "/home/ohryx";
     description = "Ohryx";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
